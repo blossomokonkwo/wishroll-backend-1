1fa033e9211762ba1832db0c6c03b3c8ba612232
Great <gboyokonkwo35@gmail.com>
2019-12-28 21:56:29 -0500

    Return view_count when calling 'feed' endpoint

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -11,7 +11,7 @@ class FeedController < ApplicationController
             post[:caption] = p.caption
             post[:created_at] = p.created_at
             post[:user_id] = p.user_id
-            #post[:view_count] = p.view_count
+            post[:view_count] = p.view_count
             post[:image_url] = url_for(p.post_image) if p.post_image.attached?
             @posts << post
         end
