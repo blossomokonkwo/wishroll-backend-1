98b7c56cc3b6c0697671d121624dd67bb807e3d0
Great <gboyokonkwo35@gmail.com>
2019-12-30 20:03:23 -0500

    Fixed issue with JSON in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,16 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     # we want to return a users username and their photos
-    let postsArray = Array.new
+    postsArray = Array.new
     for post in @user.posts
-      postsArray << post
+      post_hash = Hash.new
+      post_hash["id"] = post.id
+      post_hash["user_id"] = post.user_id
+      post_hash["created_at"] = post.created_at
+      post_hash["view_count"] = post.view_count
+      post_hash["caption"] = post.caption
+      post_hash["image_url"] = url_for post.post_image
+      postsArray << post_hash
     end
     render json: postsArray, status: :ok
   end
