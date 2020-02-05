95370f493e35bdcebcbee245842db4c7d5dfe66c
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-02-05 08:02:41 -0500

    Changed the JWT payload returned to the client

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,7 +5,7 @@ class LoginController < ApplicationController
     if @user and @user.authenticate(params[:password])
       #create session and render it to the client
       #the users username, verification status, email, profile_picture, and name are returned via the payload
-      payload = {user_id: @user.id, username: @user.username, is_verified: @user.is_verified, email: @user.email, picture: @user.profile_picture_url, first_name: @user.first_name, last_name: @user.last_name}
+      payload = {user_id: @user.id, username: @user.username, is_verified: @user.is_verified, email: @user.email, first_name: @user.first_name, last_name: @user.last_name}
       session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
       tokens = session.login
       render json: {access: tokens[:access], csrf: tokens[:csrf], access_expires_at: tokens[:access_expires_at]} , status: :ok
