44afd09f7b0fe72a5b76e73b831edd5175826840
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-02-16 19:27:36 -0500

    Fixed error in admin folder

--- app/controllers/admin/wishlists_controller.rb
+++ app/controllers/admin/wishlists_controller.rb
@@ -1,39 +1,39 @@
-class WishlistsController < Admin::BaseController 
-    def create
-        @object = Object.new(params[:object])
-        if @object.save
-          flash[:success] = "Object successfully created"
-          redirect_to @object
-        else
-          flash[:error] = "Something went wrong"
-          render 'new'
-        end
-    end
-   def update
-       @object = Object.find(params[:id])
-       if @object.update_attributes(params[:object])
-         flash[:success] = "Object was successfully updated"
-         redirect_to @object
-       else
-         flash[:error] = "Something went wrong"
-         render 'edit'
-       end
-   end
-   def show
-    @ = .find()
-end
-def index
-    @ = .all
-end
+# class WishlistsController < Admin::BaseController 
+#     def create
+#         @object = Object.new(params[:object])
+#         if @object.save
+#           flash[:success] = "Object successfully created"
+#           redirect_to @object
+#         else
+#           flash[:error] = "Something went wrong"
+#           render 'new'
+#         end
+#     end
+#    def update
+#        @object = Object.find(params[:id])
+#        if @object.update_attributes(params[:object])
+#          flash[:success] = "Object was successfully updated"
+#          redirect_to @object
+#        else
+#          flash[:error] = "Something went wrong"
+#          render 'edit'
+#        end
+#    end
+#    def show
+#     @ = .find()
+# end
+# def index
+#     @ = .all
+# end
 
-   def destroy
-       @object = Object.find(params[:id])
-       if @object.destroy
-           flash[:success] = 'Object was successfully deleted.'
-           redirect_to objects_url
-       else
-           flash[:error] = 'Something went wrong'
-           redirect_to objects_url
-       end
-   end
-end
+#    def destroy
+#        @object = Object.find(params[:id])
+#        if @object.destroy
+#            flash[:success] = 'Object was successfully deleted.'
+#            redirect_to objects_url
+#        else
+#            flash[:error] = 'Something went wrong'
+#            redirect_to objects_url
+#        end
+#    end
+# end
