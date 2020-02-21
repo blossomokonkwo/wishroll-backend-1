9acd2f9ed187b93a91941feff8eee74e3c1cb163
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-02-21 12:08:32 -0500

    Changed the profile picture aspect of the payload in the login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,7 +5,7 @@ class LoginController < ApplicationController
     if @user and @user.authenticate(params[:password])
       #create session and render it to the client
       #the users username, verification status, email, profile_picture, and name are returned via the payload
-      payload = {user_id: @user.id, username: @user.username, is_verified: @user.is_verified, email: @user.email, full_name: @user.full_name, profile_picture: url_for(@user.profile_picture) if @user.profile_picture.attached?}
+      payload = {user_id: @user.id, username: @user.username, is_verified: @user.is_verified, email: @user.email, full_name: @user.full_name, profile_picture: @user.profile_picture.attached? ? url_for(@user.profile_picture) : nil}
       session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
       tokens = session.login
       render json: {bio: @user.bio, access: tokens[:access], csrf: tokens[:csrf], access_expires_at: tokens[:access_expires_at]} , status: :ok
