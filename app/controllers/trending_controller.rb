348e37acd553c145641642040ec524feede94514
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-02-22 23:43:18 -0500

    changed the trending action

--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -2,10 +2,10 @@ class TrendingController < ApplicationController
     before_action :authorize_by_access_header!
     def trending 
         #the number of posts that are sent to the users feed page. The feed sends the most popular posts
-        @num_trending_posts = 1000
+        @num_trending_posts = 100
         @id = current_user.id
         offset = params[:offset] #as the user scrolls the offset is incremented by 100
-        @posts = Post.order(likes_count: :desc, view_count: :desc, created_at: :desc).offset(offset).limit(@num_trending_posts)
+        @posts = Post.order(likes_count: :desc, view_count: :desc, created_at: :desc).limit(@num_trending_posts).offset(offset)
         render :index, status: :ok  
     end
 end
