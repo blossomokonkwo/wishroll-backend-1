2d676a8114a3429a47bd948576cfb3a0577f76ef
Great <gboyokonkwo35@gmail.com>
2020-03-02 15:58:45 -0500

    Fixed issue with resque.rb uri parsing

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,7 +2,7 @@ require 'resque/server'
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..' #Sets rails_root to the root directory of app
 rails_env = ENV['RAILS_ENV'] || 'development' #Gets the currnet environment, or default to development  
 resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-uri = URI.parse(resque_config[rails_env])
+uri = URI.parse("localhost:6379")
 if rails_env === 'development'  
 	Resque.redis = Redis.new(host: uri.host, port: uri.port)  
 elsif rails_env === 'production'  
