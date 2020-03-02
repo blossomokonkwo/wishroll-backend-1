dcd99ed87d4965274dd468edfda7f10e4e52c0f0
Great <gboyokonkwo35@gmail.com>
2020-03-02 18:20:58 -0500

    Fixed error in application record class

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
-  class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
    
     connects_to database: { writing: :primary, reading: :primary_replica }
   end
-end
