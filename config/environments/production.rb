a3b85cd607254665983dfb20796f693a4d9f9299
Great <gboyokonkwo35@gmail.com>
2020-03-02 17:49:52 -0500

    Set up a slave database and master for use in all environments. Pointed the slave database url to replica heroku url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+
+config.active_record.database_selector = { delay: 2.seconds }
+config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
+config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,7 +1,5 @@
 require 'resque/server'
-rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..' #Sets rails_root to the root directory of app
 rails_env = ENV['RAILS_ENV'] || 'development' #Gets the currnet environment, or default to development  
-resque_config = YAML.load_file(rails_root + '/config/resque.yml')
 uri = URI.parse("localhost:6379")
 if rails_env === 'development'  
 	Resque.redis = Redis.new(host: uri.host, port: uri.port)  
