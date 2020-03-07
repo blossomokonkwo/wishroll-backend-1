013a2084472f9d0c2f2608c893beb066099e29a9
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-06 19:22:39 -0500

    Removed the before action filter that required a user to be authorized to view the trending feed

--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -1,5 +1,4 @@
 class TrendingController < ApplicationController
-    before_action :authorize_by_access_header!
     def trending 
         #the number of posts that are sent to the users feed page. The feed sends the most popular posts
         @num_trending_posts = 100
