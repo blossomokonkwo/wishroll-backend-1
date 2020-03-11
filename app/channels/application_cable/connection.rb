fa29166ae14ecd9c59d57594d0abbb647435f8a2
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-11 16:56:56 -0400

    Began to create the connection object for the action cable websocket connection. Included the jwt sessions authorization module

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,15 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    include JWTSessions::RailsAuthorization
+    identified_by :current_user
+    def connect
+       
+    end
+
+    def disconnect
+      
+    end
+    
+    
   end
 end
