efe1ed59674d464f7720db9f4805348e4e841710
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-12 03:11:25 -0400

    Check that the user requesting to recieve live streamed data is a chat room user. This check ensures that private chat rooms are kept private and that users cannot recieve messages from chat rooms that they are not apart of

--- app/channels/chat_rooms_channel.rb
+++ app/channels/chat_rooms_channel.rb
@@ -3,7 +3,7 @@ class ChatRoomsChannel < ApplicationCable::Channel
     #when a user is subscribed to a particular chatroom's channel, they recieve all the messages that are sent to that chat room. 
     #The user subscribes to this channel when they are 
     @chat_room = ChatRoom.find(params[:chat_room_id])
-      #reject unless @chat_room.users.exists?(id: current_user.id)
+    reject unless @chat_room.users.count > 0 && @chat_room.users.exists?(id: current_user.id)
     stream_for @chat_room
 
   end
