cb19145bca95909f5b89120e8e912bf9e210e41c
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-13 14:13:19 -0400

    Created the index action for the messages controller that returns all the recent messages for a given chatroom based on offset and limit values

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,6 +10,7 @@ class MessagesController < ApplicationController
             end
             if @message.save
                 MessageRelayJob.perform_later(@message)
+                #a background job should handle the pushing of notifications to users whom are members of the chat room
                 render json: nil, status: 201
             else
                 render json: {error: "The message couldn't be created"}, status: 400
@@ -18,6 +19,27 @@ class MessagesController < ApplicationController
             render json: {error: "You are unauthorized to chat in a chat room you have not formally joined"}, status: 401 
         end
     end
+
+
+    def index
+        #return all the messages for a given chat room using offset and limit constraints. As the user scrolls upwards in the chat, they should be viewing older messages.
+        #for smooth scrolling and performance the limit will be 75 messages
+        @chat_room = ChatRoom.find(params[:chat_room_id])
+        if @chat_room
+            #these will be passed in as query params
+            limit = 75 
+            offset = 0
+            @messages = @chat_room.messages.offset(0).limit(75)
+            if @messages
+                render :index, status: 200
+            else
+                render json: {error: "This chat room has no messages"}, status: 404
+            end
+        else
+            render json: {error: "This chat room does not exist"}, status: 404
+        end
+    end
+     
     
     def update
         @message = Message.find(params[:id])
---
+++ app/views/messages/index.json.jbuilder
@@ -0,0 +1,22 @@
+json.array! @messages.each do |message|
+    cache message, expires_in: 1.hour do
+        json.message do
+            json.id message.id
+            json.body message.body
+            json.kind message.kind
+            json.uuid message.uuid
+            json.media_url message.media_url
+            json.created_at message.created_at
+            json.updated_at message.updated_at
+        end
+    end
+    messenger = message.user
+    cache messenger, expires_in: 1.hour do
+        json.user do
+            json.id messenger.id
+            json.username messenger.username
+            json.profile_picture_url messenger.profile_picture_url
+            json.is_verified messenger.is_verified
+        end 
+    end
+end
