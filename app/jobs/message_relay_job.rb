902a422c6300ddf5ea919bac60b0280e0ef23cbe
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-14 14:26:07 -0400

    Convert message object to json

--- app/jobs/message_relay_job.rb
+++ app/jobs/message_relay_job.rb
@@ -2,6 +2,6 @@ class MessageRelayJob < ApplicationJob
   queue_as :messages
 
   def perform(message)
-    ChatRoomsChannel.broadcast_to(message.chat_room, message: message)
+    ChatRoomsChannel.broadcast_to(message.chat_room, message: message.to_json)
   end
 end
