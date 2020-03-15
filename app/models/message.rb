36f46181d707a3e4ccf78246a1a8eecdfa7c5a76
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-15 02:37:09 -0400

    Added the deletion of chat rooms if all the users leave the chat room. Automatically delete chat room users if a user decides to delete a chat room they created

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
     has_one_attached :media_item
-    belongs_to :chat_room, class_name: "ChatRoom", foreign_key: "chat_room_id"
-    validates :kind, presence: true, inclusion: {in: ["text", "emoji", "photo", "video", "audio"]}
+    belongs_to :chat_room, class_name: "ChatRoom", foreign_key: :chat_room_id
     belongs_to :user, -> {select([:username, :is_verified, :profile_picture_url, :id])}, class_name: "User", foreign_key: :sender_id
+    validates :chat_room, presence: true
+    validates :kind, presence: true, inclusion: {in: ["text", "emoji", "photo", "video", "audio"]}
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ApplicationRecord
-    has_many :chat_rooms, class_name: "ChatRoom",  foreign_key: :topic_id
+    has_many :chat_rooms, class_name: "ChatRoom",  foreign_key: :topic_id, dependent: :destroy
     belongs_to :user, class_name: "User", foreign_key: "user_id"
     has_one_attached :topic_image
     validates :title, presence: true, uniqueness: true, on: [:create, :update]
