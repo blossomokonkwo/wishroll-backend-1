6c3e76c88fd1264e3e99a3a715662381ad47c2c8
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-22 21:19:24 -0400

    Added the full path extension

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-    require 'profanity_filter'
+    require 'lib/tasks/profanity_filter.rb'
     before_action :authorize_by_access_header!
     def create
         @chat_room = ChatRoom.find(params[:chat_room_id])
