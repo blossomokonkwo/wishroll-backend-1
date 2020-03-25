0926988605d91023e9a2990f4161712aabcd6d3f
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-25 13:21:56 -0400

    Raise an active record validation error when a new device is being created with a token already in use

--- app/controllers/v2/devices_controller.rb
+++ app/controllers/v2/devices_controller.rb
@@ -2,7 +2,7 @@ class V2::DevicesController < ApplicationController
     before_action :authorize_by_access_header!
     def create
         #first search for a device based on the unique device token
-        @device = Device.create(device_token: params[:device_token], platform: params["platform"], user_id: current_user.id)
+        @device = Device.create!(device_token: params[:device_token], platform: params["platform"], user_id: current_user.id)
         if @device
         render json: nil, status: 201
         else
