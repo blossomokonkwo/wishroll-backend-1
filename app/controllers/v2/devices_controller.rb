de33464a47a3837004f3d1579e63345ba2b940a1
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-25 12:51:43 -0400

    Changed the method used to create a new device

--- app/controllers/v2/devices_controller.rb
+++ app/controllers/v2/devices_controller.rb
@@ -2,8 +2,7 @@ class V2::DevicesController < ApplicationController
     before_action :authorize_by_access_header!
     def create
         #first search for a device based on the unique device token
-        @user = current_user
-        @device = @user.device.create(device_token: params[:device_token], platform: params["platform"])
+        @device = current_user.create_device(device_token: params[:device_token], platform: params["platform"])
         if @device
         render json: nil, status: 201
         else
