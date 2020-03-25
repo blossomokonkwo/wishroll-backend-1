e44778caa366f0be466275f08a782ae53527d0ab
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-25 13:39:07 -0400

    Created comment that explains the unhandled error

--- app/controllers/v2/devices_controller.rb
+++ app/controllers/v2/devices_controller.rb
@@ -3,6 +3,7 @@ class V2::DevicesController < ApplicationController
     def create
         #first search for a device based on the unique device token
         @device = Device.create!(device_token: params[:device_token], platform: params["platform"], user_id: current_user.id)
+        #if the device ha already been created then that indicates that the user already has a registered device with their account
         if @device
         render json: nil, status: 201
         else
