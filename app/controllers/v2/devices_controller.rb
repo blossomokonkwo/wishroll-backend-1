ed6bdc62e9fdc1e3e15d8fa1088b841ac134f748
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-25 12:47:41 -0400

    Find user based on the current user

--- app/controllers/v2/devices_controller.rb
+++ app/controllers/v2/devices_controller.rb
@@ -2,7 +2,7 @@ class V2::DevicesController < ApplicationController
     before_action :authorize_by_access_header!
     def create
         #first search for a device based on the unique device token
-        @user = User.find(params[:user_id])
+        @user = current_user
         @device = @user.device.create(device_token: params[:device_token], platform: params["platform"])
         if @device
         render json: nil, status: 201
