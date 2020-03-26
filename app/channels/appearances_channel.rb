1936d6cb1a86942cc42a32d3fd5c08f6e4df37c5
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-03-26 12:11:01 -0400

    Changed the parameter name from data to args in the appearance and disappearance actions in the appearances_channel

--- app/channels/appearances_channel.rb
+++ app/channels/appearances_channel.rb
@@ -15,25 +15,25 @@ class AppearancesChannel < ApplicationCable::Channel
     stop_all_streams
   end
 
-  def appearance(data)
+  def appearance(args)
     #this action is called by the client when a user has entered a specific chat room
     #the data field is hash that contains information of the users appearance such as the chat room id of the appearance and the timestamp of the appearance
-    chat_room_user = ChatRoomUser.find_by(user_id: current_user.id, chat_room_id: data[:chat_room_id])
+    chat_room_user = ChatRoomUser.find_by(user_id: current_user.id, chat_room_id: args[:chat_room_id])
     if chat_room_user #if the current user is apart of the specified chat room
       chat_room_user.appearance = true
       chat_room_user.save
-      chat_room = ChatRoom.find(data[:chat_room_id])
+      chat_room = ChatRoom.find(args[:chat_room_id])
       AppearancesChannel.broadcast_to(chat_room, current_user.username, {appearance: true})
     end
   end
 
-  def disappearance(data)
+  def disappearance(args)
     #this action is called by the client when a user has left a specific chat room 
-    chat_room_user = ChatRoomUser.find_by(user_id: current_user.id, chat_room_id: data[:chat_room_id])
+    chat_room_user = ChatRoomUser.find_by(user_id: current_user.id, chat_room_id: args[:chat_room_id])
     if chat_room_user
       chat_room_user.appearance = false
       chat_room_user.save
-      chat_room = ChatRoom.find(data[:chat_room_id])
+      chat_room = ChatRoom.find(args[:chat_room_id])
       AppearancesChannel.broadcast_to(chat_room, current_user.username, {appearance: false})
     end
   end
@@ -42,17 +42,15 @@ class AppearancesChannel < ApplicationCable::Channel
     #check to see if the typer isn't the current user and the user isn't appearing in the chat room and the user
     #if all of these criteria are met, then send a typing broadcast to all of the users in the app who meet the away criteria.
     #the app should display a typing indicator under the user who is typing on the app to indicate that a specific user is typing for those in the chat room
-    typing_user = User.find(data[:typing_user_id])
     chat_room = ChatRoom.find(data[:chat_room_id])
     if typing_user and chat_room
-      AppearancesChannel.broadcast_to(chat_room, typing_user.username, {typing: true})
+      AppearancesChannel.broadcast_to(chat_room, current_user.username, {typing: true})
     end
     
   end
 
   def end_typing(data)
     #when the user has ended typing, a broadcast should be sent to indicate that the user has ended typing and that the typing indicator shouldn't be activated under that specific user
-    end_typing_user = User.find(data[:end_typing_user_id])
     chat_room = ChatRoom.find(data[:chat_room_id])
     if end_typing_user and chat_room
       AppearancesChannel.broadcast_to(chat_room, end_typing_user.username, {typing: false})
