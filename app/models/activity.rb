0359bb180da4d74d32da3f47eacac0a2035f470a
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-04-09 23:48:55 -0400

    Perform Async

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -8,6 +8,6 @@ class Activity < ApplicationRecord
   after_create do
     #we want to delete instances of the activity class after 3 days.
     ActivityCleanUpWorker.perform_in(3.days, self.id)
-    ActivityNotificationWorker.perform_in(1.second, self.id)
+    ActivityNotificationWorker.perform_async(self.id)
   end
 end
