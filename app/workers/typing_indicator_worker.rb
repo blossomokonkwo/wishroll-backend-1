b75478dba16a9dca0a02ba3c896f5c07fbac2f7c
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-04-10 01:12:44 -0400

    Added info in worker

--- app/workers/typing_indicator_worker.rb
+++ app/workers/typing_indicator_worker.rb
@@ -3,6 +3,7 @@ class TypingIndicatorWorker
     sidekiq_options queue: "notifications", retry: false
     def perform(user_id, chat_room_id)
         typer = User.find user_id
+        chat_room = ChatRoom.find(chat_room_id)
         chat_room_users = ChatRoomUser.where(chat_room_id: chat_room_id).includes(:user)
         if chat_room_users.any?
             chat_room_users.each do |chat_room_user|
@@ -13,7 +14,11 @@ class TypingIndicatorWorker
                         notification = Rpush::Apns::Notification.new
                         notification.app = Rpush::Client::ActiveRecord::App.find_by_name("wishroll-ios")
                         notification.device_token = device.device_token
-                        notification.alert = "#{typer.username} is typing..."
+                        if chat_room.name
+                            notification.alert = "#{chat_room.name}\n#{typer.username} is typing..."
+                        else
+                            notification.alert = "#{typer.username} is typing..."
+                        end
                         notification.sound = 'sosumi.aiff'
                         notification.data = {}
                         notification.save!
