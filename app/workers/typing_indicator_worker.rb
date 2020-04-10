43a3c4970010bde8dddb297933eb3a0168de7588
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-04-10 01:05:21 -0400

    Added workers that notify users when a user joins a chatroom and starts typing

---
+++ app/workers/typing_indicator_worker.rb
@@ -0,0 +1,26 @@
+class TypingIndicatorWorker
+    include Sidekiq::Worker
+    sidekiq_options queue: "notifications", retry: false
+    def perform(user_id, chat_room_id)
+        user = User.find user_id
+        chat_room_users = ChatRoomUser.where(chat_room_id: chat_room_id).includes(:user)
+        if chat_room_users.any?
+            chat_room_users.each do |chat_room_user|
+                unless chat_room_user.muted
+                    user = chat_room_user
+                    device = user.current_device
+                    if device
+                        notification = Rpush::Apns::Notification.new
+                        notification.app = Rpush::Client::ActiveRecord::App.find_by_name("wishroll-ios")
+                        notification.device_token = device.device_token
+                        notification.alert = "#{user.username} is typing..."
+                        notification.sound = 'sosumi.aiff'
+                        notification.data = {}
+                        notification.save!
+                        Rpush.push
+                    end
+                end
+            end
+        end
+    end
+end
