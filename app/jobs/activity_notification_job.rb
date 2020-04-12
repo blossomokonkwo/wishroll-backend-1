ac41c9ea35729e9fc9718b1443ca94c153cbe579
Great <gboyokonkwo35@gmail.com>
2020-04-12 17:36:31 -0400

    Check for current device to avoid exceptions

--- app/jobs/activity_notification_job.rb
+++ app/jobs/activity_notification_job.rb
@@ -6,19 +6,21 @@ class ActivityNotificationJob < ApplicationJob
             user = User.find(activity.user_id)
             n = Rpush::Apns::Notification.new
             n.app = Rpush::Client::ActiveRecord::App.find_by_name("wishroll-ios")
-            n.device_token = user.current_device.device_token
-            if activity.activity_type == "Comment"
-                comment_body = Comment.find(activity.content_id).body
-                n.alert = "#{activity.activity_phrase}: #{comment_body}"
-            elsif  activity.activity_type == "Post"
-                n.alert = "#{activity.activity_phrase} 💕\n#{activity.post_url}"
-            else
-                n.alert = "#{activity.activity_phrase}"
+            if user.current_device
+                n.device_token = user.current_device.device_token
+                if activity.activity_type == "Comment"
+                    comment_body = Comment.find(activity.content_id).body
+                    n.alert = "#{activity.activity_phrase}: #{comment_body}"
+                elsif  activity.activity_type == "Post"
+                    n.alert = "#{activity.activity_phrase} 💕\n#{activity.post_url}"
+                else
+                    n.alert = "#{activity.activity_phrase}"
+                end
+                n.sound = 'sosumi.aiff'
+                n.data = {}
+                n.save!
+                Rpush.push
             end
-            n.sound = 'sosumi.aiff'
-            n.data = {}
-            n.save!
-            Rpush.push
         end        
     end
 end
