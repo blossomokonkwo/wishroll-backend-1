746fb6ff6aeb6b69335f02ba0cc2ff34a225b378
Great <gboyokonkwo35@gmail.com>
2020-04-12 19:02:48 -0400

    Improve Job efficiency

--- app/jobs/join_chat_room_job.rb
+++ app/jobs/join_chat_room_job.rb
@@ -2,7 +2,8 @@ class JoinChatRoomNotificationJob < ApplicationJob
     queue_as :notifications
     def perform(user_id, chat_room_id)
         joined_user = User.find(user_id)
-        chat_room_users = ChatRoomUser.where(chat_room_id: chat_room_id).includes(:user)
+        app = Rpush::Client::ActiveRecord::App.find_by_name("wishroll-ios")
+        chat_room_users = ChatRoomUser.where(chat_room_id: chat_room_id).includes([:user])
         chat_room = ChatRoom.find(chat_room_id)
         if chat_room_users.any?
             chat_room_users.each do |chat_room_user|
@@ -12,7 +13,7 @@ class JoinChatRoomNotificationJob < ApplicationJob
                         device = user.current_device
                         if device
                             notification = Rpush::Apns::Notification.new
-                            notification.app = Rpush::Client::ActiveRecord::App.find_by_name("wishroll-ios")
+                            notification.app = app
                             notification.device_token = device.device_token
                             if chat_room.name
                                 notification.alert = "#{chat_room.name}\n#{joined_user.username} has joined the chat... 😳"
