80a82d4bd4e5815eeffea9af364af0875523d415
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-04-13 12:48:17 -0400

    Fine tuned the routes

--- config/routes.rb
+++ config/routes.rb
@@ -83,8 +83,8 @@ Rails.application.routes.draw do
   post 'search', to: "search#search"
   post 'search-posts', to: 'search#search_posts'
   post 'search-accounts', to: 'search#search_accounts'
-  post 'search-followers', to: 'search#search_followers'
-  post 'search-followed-users', to: 'search_followed_users#search'
+  post 'search-followers/:username', to: 'search#search_followers'
+  post 'search-followed-users/:username', to: 'search#search_followed_users'
   #post 'search-topics-and-chatrooms', to: "search_chat_rooms_and_topics#search" version 3
   delete 'logout', to: "logout#destroy" #the logout route
   post 'refresh', to: "refresh#create" #the refresh controller where refresh tokens are returned
