0b2e4dce9296d94c257c50870cd1eae45b74b710
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-04-17 22:27:47 -0400

    Using the first method instead of limit

--- app/controllers/v2/devices_controller.rb
+++ app/controllers/v2/devices_controller.rb
@@ -2,7 +2,7 @@ class V2::DevicesController < ApplicationController
     before_action :authorize_by_access_header!
     def create
         #if the device ha already been created then that indicates that the user already has a registered device with their account
-        @device = current_user.devices.where(device_token: params[:device_token], platform: params["platform"]).limit(1)
+        @device = current_user.devices.where(device_token: params[:device_token], platform: params["platform"]).first
         if @device
             old_current_device = current_user.current_device    #devices.where(current_device: true).first.current_device
             if !old_current_device or old_current_device.device_token != @device.device_token
