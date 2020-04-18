6fa6f76175a93979e6a08228bcfcc4c271008586
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-04-17 22:51:35 -0400

    Improved the logic in the devices controller create action

--- app/controllers/v2/devices_controller.rb
+++ app/controllers/v2/devices_controller.rb
@@ -3,14 +3,20 @@ class V2::DevicesController < ApplicationController
     def create
         #if the device ha already been created then that indicates that the user already has a registered device with their account
         @device = current_user.devices.where(device_token: params[:device_token], platform: params["platform"]).first
-        if @device
-            old_current_device = current_user.current_device    #devices.where(current_device: true).first.current_device
-            if !old_current_device or old_current_device.device_token != @device.device_token
+        old_current_device = current_user.current_device    
+        if @device           
+            if !old_current_device 
+                if old_current_device.device_token != @device.device_token
+                    old_current_device.update!(current_device: false)
+                end
                 @device.current_device = true
                 @device.save
             end
             render json: {error: "This device has already been created"}, status: 500
         else
+            if old_current_device
+                old_current_device.update!(current_device: false)
+            end
             @device = current_user.devices.create!(device_token: params[:device_token], platform: params[:platform], current_device: true)
             render json: nil, status: :created
         end
