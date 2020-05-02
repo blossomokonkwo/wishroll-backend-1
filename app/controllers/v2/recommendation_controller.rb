8484eca2587a25d36d079f5524af9c1387880a94
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-05-02 07:51:09 -0400

    Improved the recommended videos api

--- app/controllers/v2/recommendation_controller.rb
+++ app/controllers/v2/recommendation_controller.rb
@@ -9,18 +9,29 @@ through the page.
 =end
     def recommend_videos
         #recommend only videos
-        limit = 10
+        @tags = params[:tags]
+        limit = 50 / @tags.count
+        limit += 1 if limit == 0
+        counter = 0
         offset = params[:offset] #how many posts to skip before returning the new data
         @recommended_video_posts = Array.new
         @post = Post.find(params[:post_id]) #the reference post for the recommendations
         @post_user = @post.user #the reference post user
         #loop through the tags of the video in order to derive their associated posts
-        Tag.where("text ILIKE ?", "%#{params[:text]}%").order(view_count: :desc, likes_count: :desc, created_at: :desc, id: :asc).offset(offset).limit(limit).find_each do |tag|
-            post = tag.post
-            url = post.posts_media_url
-            #only add the post to the array if it is in a movie format(mp4, mov)
-            @recommended_video_posts << post if !current_user.reported_posts.include?(post) and (url.end_with?("mov") or url.end_with?("mp4")) and (post.id != @post.id)
-        end
+        if @tags.any?
+            @tags.each do |tag|
+                if counter > 50
+                    break
+                end
+                Tag.where("text ILIKE ?", "%#{tag}%").order(view_count: :desc, likes_count: :desc, created_at: :desc, id: :asc).offset(offset).limit(limit).find_each do |tag|
+                    post = tag.post
+                    url = post.posts_media_url
+                    #only add the post to the array if it is in a movie format(mp4, mov)
+                    @recommended_video_posts << post if !current_user.reported_posts.include?(post) and (url.end_with?("mov") or url.end_with?("mp4")) and (post.id != @post.id)
+                end
+                counter += 1
+            end
+        end 
         if @recommended_video_posts.any? 
             #sort the recommended posts so that the creator of the reference post has his or her content more prevalant.
             @recommended_video_posts.sort! do |a,b|
