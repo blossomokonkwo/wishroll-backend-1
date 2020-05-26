1baefea71fd4afa373fc66e2b5a27e692f9c6e08
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-05-26 07:13:33 -0400

    Added removal of old index

--- db/migrate/20200526110752_add_unique_index_to_likes.rb
+++ db/migrate/20200526110752_add_unique_index_to_likes.rb
@@ -1,5 +1,6 @@
 class AddUniqueIndexToLikes < ActiveRecord::Migration[6.0]
   def change
+    remove_index :likes, name: :index_likes_on_likeable_type_and_likeable_id
     add_index :likes, [:user_id, :likeable_id, :likeable_type], :unique => true
   end
 end
