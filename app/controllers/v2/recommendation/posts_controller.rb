0ccf7a206ece1596a4ba0f0579edabfff1013ce1
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-06-02 16:03:31 -0400

    Improved Reccommedations

--- app/controllers/v2/recommendation/posts_controller.rb
+++ app/controllers/v2/recommendation/posts_controller.rb
@@ -9,7 +9,13 @@ class V2::Recommendation::PostsController < ApplicationController
         english_articles = ["the", "a", "when" "some", "they", "back", "because", "if", "in"]
         @post.tags.pluck(:text).map {|word| word.split(' ').delete_if {|word| english_articles.include?(word)}.map {|t| keywords << "%#{t}%"}}
         caption_terms = @post.caption.split(' ').delete_if {|word| english_articles.include?(word)}.map {|word| keywords << "%#{word}%"}
-        @posts = Post.joins(:tags).where("tags.text ILIKE ANY (array[?]) or posts.caption ILIKE ANY (array[?])", keywords, keywords).distinct.includes([user: :blocked_users]).order(likes_count: :desc, view_count: :desc, id: :asc).offset(offset).limit(limit).to_a
+        @posts = Array.new
+        if @post.media_url.end_with?("mov") || @post.media_url.end_with?("mp4") || @post.media_url.end_with?("m4v") || @post.media_url.end_with?("flv")
+            video_formats = ["%mov%", "%mp4%", "%m4v%", "%flv%"]
+            @posts = Post.joins(:tags).where("posts.media_url LIKE ANY (array[?]) AND tags.text ILIKE ANY (array[?]) or posts.caption ILIKE ANY (array[?])",video_formats, keywords, keywords).distinct.includes([user: :blocked_users]).order(likes_count: :desc, view_count: :desc, id: :asc).offset(offset).limit(limit).to_a
+        else
+            @posts = Post.joins(:tags).where("tags.text ILIKE ANY (array[?]) or posts.caption ILIKE ANY (array[?])",keywords, keywords).distinct.includes([user: :blocked_users]).order(likes_count: :desc, view_count: :desc, id: :asc).offset(offset).limit(limit).to_a
+        end
         @posts.delete_if do |p|
             p.id == @post.id or current_user.reported_posts.include?(p) or p.user.blocked_users.include?(current_user) or current_user.blocked_users.include?(p.user)
         end
@@ -19,5 +25,6 @@ class V2::Recommendation::PostsController < ApplicationController
             render json: nil, status: :not_found
         end
     end
+    
     
 end
