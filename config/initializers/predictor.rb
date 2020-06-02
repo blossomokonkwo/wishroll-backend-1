adc6b8a647cb852500b2629b3d6c2b112c19db2a
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-06-02 12:57:33 -0400

    Removed hiredis gem

--- config/initializers/predictor.rb
+++ config/initializers/predictor.rb
@@ -1,3 +1,3 @@
 
-Predictor.redis = Redis.new(url: ENV['PREDICTOR_REDIS_URL'], driver: :hidredis) if Rails.env.production?
+Predictor.redis = Redis.new(url: ENV['PREDICTOR_REDIS_URL']) if Rails.env.production?
 Predictor.redis = Redis.new if Rails.env.development?
