1f8df0c591a32d60b7d59d7a6e1ad39ecaf3ffa0
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-06-05 16:36:46 -0400

    Finally discovered the query to retrieve all the posts created by a users followed users

--- app/controllers/v2/feed/posts_controller.rb
+++ app/controllers/v2/feed/posts_controller.rb
@@ -1,8 +1,9 @@
 class V2::Feed::PostsController < ApplicationController
+    before_action :authorize_by_access_header!
     def feed
         offset = params[:offset]
         limit = 25
-        @posts = Post.order(id: :asc).offset(offset).limit(limit)
+        @posts = Post.where(user: current_user.followed_users).order(created_at: :desc).offset(offset).limit(limit)
         if @posts.any?
             render :index, status: :ok
         else
