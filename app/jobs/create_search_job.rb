f6c4ac2ef4c1f05c91edcb428c8142463ca14acf
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-06-09 17:15:18 -0400

    Increased the number of posts returned by the search controller

--- app/jobs/create_search_job.rb
+++ app/jobs/create_search_job.rb
@@ -3,14 +3,16 @@ class CreateSearchJob < ApplicationJob
         ActiveRecord::Base.connected_to(role: :writing) do
             begin
                 search = Search.create!(query: query, result_type: result_type)
+                CreateLocationJob.perform_now(ip_address, timezone, search.id, search.class.name)
             rescue => exception
                 search = Search.find_by(query: query, result_type: result_type)
                 if search
                     search.occurences += 1
                     search.save!
+                    CreateLocationJob.perform_now(ip_address, timezone, search.id, search.class.name)
                 end
             end
-            CreateLocationJob.perform_now(ip_address, timezone, search.id, search.class.name)
+            
         end
     end
     
