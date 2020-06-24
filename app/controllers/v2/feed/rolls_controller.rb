08c8786353638a8978f1ecf7a343d2adf18c5516
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-06-24 13:06:52 -0400

    Convert the followed users association to an array

--- app/controllers/v2/feed/rolls_controller.rb
+++ app/controllers/v2/feed/rolls_controller.rb
@@ -4,7 +4,7 @@ class V2::Feed::RollsController < ApplicationController
         offset = params[:offset]
         @id = current_user.id
         limit = 12
-        feed_users = current_user.followed_users
+        feed_users = current_user.followed_users.to_a
         feed_users << current_user
         @rolls = Roll.where(user: feed_users).order(created_at: :desc).offset(offset).limit(limit)
         if @rolls.any?
