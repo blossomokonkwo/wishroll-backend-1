725aac6e9482276d496238cdca973e12d3a2c807
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-06-27 19:38:54 -0400

    Made improvements to the device controller

--- app/controllers/v2/devices_controller.rb
+++ app/controllers/v2/devices_controller.rb
@@ -2,15 +2,15 @@ class V2::DevicesController < ApplicationController
     before_action :authorize_by_access_header!
     def create
         #if the device ha already been created then that indicates that the user already has a registered device with their account
-        @device = current_user.devices.find_by(device_token: params[:device_token], platform: params["platform"])
+        @device = current_user.devices.find_by(device_token: params[:device_token], platform: params[:platform])
         old_current_device = current_user.current_device    
         if @device           
             if old_current_device and old_current_device != @device
                 old_current_device.current_device = false
                 old_current_device.save
-            end
-            @device.current_device = true
-            @device.save
+                @device.current_device = true
+                @device.save
+            end 
             render json: {error: "This device has already been created"}, status: 500
         else
             if old_current_device
