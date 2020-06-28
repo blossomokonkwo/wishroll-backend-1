39278e1b5f3b461b8d5e9cd1a637e435cfd79760
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-06-27 22:03:28 -0400

    Fixed error in Relationship class

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,8 +3,8 @@ class Relationship < ApplicationRecord
   belongs_to :follower_user, class_name: "User", foreign_key: :follower_id, counter_cache: :following_count
 
   after_create do
-    unless Activity.find_by(content_id: id, active_user_id: follower_id, user_id: followed_id, activity_type: class.name)
-      Activity.create(content_id: id, active_user: follower_user, user: followed_user, activity_type: class.name, activity_phrase: "#{follower_user.username} began following you")
+    unless Activity.find_by(content_id: id, active_user_id: follower_id, user_id: followed_id, activity_type: self.class.name)
+      Activity.create(content_id: id, active_user: follower_user, user: followed_user, activity_type: self.class.name, activity_phrase: "#{follower_user.username} began following you")
     end
   end
 end
