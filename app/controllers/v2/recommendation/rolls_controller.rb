8df5d198a415a9f9e9ab30591d6262ca91747073
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-06-28 02:56:29 -0400

    Fixed the rolls recommendation controller action

--- app/controllers/v2/recommendation/rolls_controller.rb
+++ app/controllers/v2/recommendation/rolls_controller.rb
@@ -11,7 +11,7 @@ class V2::Recommendation::RollsController < ApplicationController
         caption_terms = @roll.caption.split(' ').delete_if {|word| english_articles.include?(word)}.map {|word| keywords << "%#{word}%"}
         @rolls = Roll.joins(:tags).where("tags.text ILIKE ANY (array[?]) OR rolls.caption ILIKE ANY (array[?])", keywords, keywords).distinct.includes([user: :blocked_users]).order(likes_count: :desc, view_count: :desc, id: :asc).offset(offset).limit(limit).to_a
         @rolls.delete_if do |p|
-            p.id == @roll.id or current_user.reported_rolls.include?(p) or p.user.blocked_users.include?(current_user) or current_user.blocked_users.include?(p.user)
+            p.id == @roll.id or p.user.blocked_users.include?(current_user) or current_user.blocked_users.include?(p.user)
         end
         if @rolls.any?
             render :index, status: :ok
