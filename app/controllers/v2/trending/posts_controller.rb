c840b0abad647699c63f26368c432b6933b5ce4e
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-07-09 12:33:43 -0400

    Changed the trending posts

--- app/controllers/v2/trending/posts_controller.rb
+++ app/controllers/v2/trending/posts_controller.rb
@@ -4,7 +4,7 @@ class V2::Trending::PostsController < ApplicationController
         #the number of posts that are sent to the users feed page. The feed sends the most popular posts
         limit = 18
         offset = params[:offset] #as the user scrolls the offset is incremented by 100
-        @posts = Post.includes([:user]).order(popularity_rank: :desc).offset(offset).limit(limit)
+        @posts = User.find(4).posts.order(created_at: :desc).offset(offset).limit(limit)
         if @posts.any?
             @current_user = current_user
             render :index, status: :ok  
