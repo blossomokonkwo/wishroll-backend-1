00e312d9a404bdcaca4d301933f85a1e94c4beae
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-07-18 01:37:59 -0400

    Fixed error in recommendation controller

--- app/controllers/v2/recommendation/posts_controller.rb
+++ app/controllers/v2/recommendation/posts_controller.rb
@@ -10,7 +10,7 @@ class V2::Recommendation::PostsController < ApplicationController
         caption_terms = @post.caption.split(' ').delete_if {|word| english_articles.include?(word)}.map {|word| keywords << "#{word} "}
         @posts = Post.fetch_multi(Tag.search(keywords).limit(limit).offset(offset).pluck(:post_id)).uniq {|p| p.id}.delete_if {|p| p.id == params[:post_id]}
         if @post.media_url.ends_with?("mov") or @post.media_url.ends_with?("mp4") or @post.media_url.ends_with?("mv4") or @post.media_url.ends_with?("m4v")
-            @post.delete_if do |p|
+            @posts.delete_if do |p|
                 p.media_url.ends_with?("mov") or p.media_url.ends_with?("mp4") or p.media_url.ends_with?("mv4") or p.media_url.ends_with?("m4v")
             end
         end
