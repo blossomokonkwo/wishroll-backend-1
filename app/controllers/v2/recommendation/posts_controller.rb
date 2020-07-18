ffb5123e6cc06aff9eeaf7475f9f1a47280a00ff
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-07-18 00:38:24 -0400

    Added a uniqueness to the posts returned in the recommended posts

--- app/controllers/v2/recommendation/posts_controller.rb
+++ app/controllers/v2/recommendation/posts_controller.rb
@@ -8,7 +8,7 @@ class V2::Recommendation::PostsController < ApplicationController
         english_articles = ["the", "a", "when" "some", "they", "back", "because", "if", "in"]
         @post.fetch_tags.pluck(:text).map {|word| word.split(' ').delete_if {|word| english_articles.include?(word)}.map {|t| keywords << "#{t} "}}
         caption_terms = @post.caption.split(' ').delete_if {|word| english_articles.include?(word)}.map {|word| keywords << "#{word} "}
-        @posts = Post.fetch_multi(Tag.search(keywords).limit(limit).offset(offset).pluck(:post_id)).distinct
+        @posts = Post.fetch_multi(Tag.search(keywords).limit(limit).offset(offset).pluck(:post_id)).uniq! {|p| p.id}
         if @posts.any?
             @current_user = current_user
             render :index, status: :ok
