a4a66976ac2fc8823ed60b0c57215c05f3fb17b1
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-07-18 19:14:53 -0400

    Fixed error in comments class that used the wrong cache key when returning whether a comment was liked or not. Added the sort only flag to the the tag search scope

--- app/models/like.rb
+++ app/models/like.rb
@@ -7,19 +7,12 @@ class Like < ApplicationRecord
     #we want to update the likeable's updated at date when a like is being destroyed. This invalidates the cache for the likeable and forces a DB read.
     logger.debug {"[WishRoll Cache] delete succeeded for WishRoll:Cache:Like:Liker:#{user.id}:Liked:#{likeable.uuid}"} if Rails.cache.delete("WishRoll:Cache:Like:Liker:#{user.id}:Liked:#{likeable.uuid}")
     likeable.touch
-    # if likeable_type == "Post"
-    #   Post.find(likeable.id).touch
-    # elsif likeable_type == "Comment"
-    #   Comment.find(likeable.id).touch
-    # else
-    #   Roll.find(likeable.id).touch
-    # end
   end
 
   after_create :update_likeable
 
   def update_likeable
-    Rails.cache.write("WishRoll:Cache:Like:Liker:#{user.id}:Liked:#{likeable.uuid}",true)
+    logger.debug {"[WishRoll Cache] write succeeded for WishRoll:Cache:Like:Liker:#{user.id}:Liked:#{likeable.uuid}"} if Rails.cache.write("WishRoll:Cache:Like:Liker:#{user.id}:Liked:#{likeable.uuid}", true)
     #write the boolean value of true whenever a like is created 
      #we want change the updated at date of the post model when it has been liked. This is because the after commit callback isn't called when the counter_cache counters are updated (the likes count)
     active_user = user
