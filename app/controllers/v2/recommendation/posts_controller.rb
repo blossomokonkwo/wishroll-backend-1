081a4f549b034e52b39eb48d9913a34b08d7e858
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-07-18 00:17:29 -0400

    Use full text search for the recommendation controller and use basic search for the user search

--- app/controllers/v2/recommendation/posts_controller.rb
+++ app/controllers/v2/recommendation/posts_controller.rb
@@ -4,14 +4,11 @@ class V2::Recommendation::PostsController < ApplicationController
         limit = 15
         offset = params[:offset]
         @post = Post.fetch(params[:post_id])
-        keywords = Array.new
+        keywords = String.new
         english_articles = ["the", "a", "when" "some", "they", "back", "because", "if", "in"]
-        @post.fetch_tags.pluck(:text).map {|word| word.split(' ').delete_if {|word| english_articles.include?(word)}.map {|t| keywords << "%#{t}%"}}
-        caption_terms = @post.caption.split(' ').delete_if {|word| english_articles.include?(word)}.map {|word| keywords << "%#{word}%"}
-        @posts = Post.joins(:tags).where("tags.text ILIKE ANY (array[?]) OR posts.caption ILIKE ANY (array[?])", keywords, keywords).distinct.includes([user: :blocked_users]).order(likes_count: :desc, view_count: :desc, id: :asc).offset(offset).limit(limit).to_a
-        @posts.delete_if do |p|
-            p.id == @post.id or p.user.blocked?(current_user) or current_user.blocked?(p.user)
-        end
+        @post.fetch_tags.pluck(:text).map {|word| word.split(' ').delete_if {|word| english_articles.include?(word)}.map {|t| keywords << "#{t} "}}
+        caption_terms = @post.caption.split(' ').delete_if {|word| english_articles.include?(word)}.map {|word| keywords << "#{word} "}
+        @posts = Post.fetch_multi(Tag.search().pluck(:post_id))
         if @posts.any?
             @current_user = current_user
             render :index, status: :ok
