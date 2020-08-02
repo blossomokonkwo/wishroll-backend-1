23578bba182125010abe89e2def395ca67b40caf
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-08-02 01:16:18 -0400

    Allow users to search the relationships of other users

--- app/controllers/v2/search/relationships_controller.rb
+++ app/controllers/v2/search/relationships_controller.rb
@@ -3,17 +3,22 @@ class V2::Search::RelationshipsController < ApplicationController
     def search
         offset = params[:offset]
         limit = 12
-        if params[:followers]
-            @users = current_user.follower_users.where('username ILIKE ? OR name ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order(followers_count: :desc, verified: :desc).offset(offset).limit(limit)
-        else
-            @users = current_user.followed_users.where('username ILIKE ? OR name ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order(followers_count: :desc, verified: :desc).offset(offset).limit(limit)
-        end
-        if @users.any?
-            @current_user = current_user
-            render :index, status: :ok
-        else
+        @user = User.fetch(params[:user_id])
+        if @user
+            if params[:followers]
+                @users = @user.follower_users.where('username ILIKE ? OR name ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order(followers_count: :desc, verified: :desc).offset(offset).limit(limit)
+            else
+                @users = @user.followed_users.where('username ILIKE ? OR name ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order(followers_count: :desc, verified: :desc).offset(offset).limit(limit)
+            end
+            if @users.any?
+                @current_user = current_user
+                render :index, status: :ok
+            else
+                render json: nil, status: :not_found
+            end
+         else
             render json: nil, status: :not_found
-        end
+         end
     end
     
 end
