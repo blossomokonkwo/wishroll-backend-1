39f1be9dc72227372d33c10a4227aff42a0a52ee
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-08-02 01:25:27 -0400

    Cast the users results from the search to an array object

--- app/controllers/v2/search/relationships_controller.rb
+++ app/controllers/v2/search/relationships_controller.rb
@@ -6,9 +6,9 @@ class V2::Search::RelationshipsController < ApplicationController
         @user = User.fetch(params[:user_id])
         if @user
             if params[:followers]
-                @users = @user.follower_users.where('username ILIKE ? OR name ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order(followers_count: :desc, verified: :desc).offset(offset).limit(limit)
+                @users = @user.follower_users.where('username ILIKE ? OR name ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order(followers_count: :desc, verified: :desc).offset(offset).limit(limit).to_a
             else
-                @users = @user.followed_users.where('username ILIKE ? OR name ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order(followers_count: :desc, verified: :desc).offset(offset).limit(limit)
+                @users = @user.followed_users.where('username ILIKE ? OR name ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order(followers_count: :desc, verified: :desc).offset(offset).limit(limit).to_a
             end
             if @users.any?
                 @current_user = current_user
