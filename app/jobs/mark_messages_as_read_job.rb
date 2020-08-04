5501c0b65fb1cc4b94fe0e142019966787d1609f
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-08-04 02:37:21 -0400

    Make the mark messages read job more efficient

--- app/jobs/mark_messages_as_read_job.rb
+++ app/jobs/mark_messages_as_read_job.rb
@@ -2,8 +2,8 @@ class MarkMessagesAsReadJob < ApplicationJob
     def perform(chat_room_id, user_id)
         user = User.fetch(user_id)
         chat_room = ChatRoom.find(chat_room_id)
-        chat_room.messages.find_each do |message|
-            message.mark_as_read! for: user if !message.read?(user)
+        Message.unread_by(user).where(chat_room: chat_room).find_each do |message|
+            message.mark_as_read! for: user
         end
         chat_room.touch
     end
