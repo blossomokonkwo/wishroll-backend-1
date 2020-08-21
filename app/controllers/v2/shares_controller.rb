1e8bce5b493a377666d2f83c8f6606d7c4487d88
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-08-21 05:10:19 -0400

    Undo the last commit

--- app/controllers/v2/shares_controller.rb
+++ app/controllers/v2/shares_controller.rb
@@ -4,18 +4,18 @@ class V2::SharesController < ApplicationController
         if params[:roll_id] and roll = Roll.find(params[:roll_id])
             begin
                 share = roll.shares.create!(user: current_user, shared_service: params[:shared_service])
-                render json: nil, status: :created
                 ShareActivityJob.perform_now(current_user_id: current_user.id, share_id: share.id)
                 UpdateWishrollScoreJob.perform_now(roll.user.id, 3)
+                render json: nil, status: :created
             rescue => exception
                 render json: {error: "Could not share the roll: #{roll.errors.inspect}"}, status: :bad_request
             end            
         elsif params[:post_id] and post = Post.find(params[:post_id])
             begin
                 share = post.shares.create!(user: current_user, shared_service: params[:shared_service])
-                render json: nil, status: :created
                 ShareActivityJob.perform_now(current_user_id: current_user.id, share_id: share.id)
                 UpdateWishrollScoreJob.perform_now(post.user.id, 3)
+                render json: nil, status: :created
             rescue => exception
                 render json: {error: "Could not share the post: #{post}"}, status: :bad_request
             end
