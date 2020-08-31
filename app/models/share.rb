ea6bc6581b91ee10dd0467f54f83a5728ce3b18a
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-08-31 18:17:07 -0400

    Changed the any word option in the search scope from true to false

--- app/models/share.rb
+++ app/models/share.rb
@@ -3,7 +3,7 @@ class Share < ApplicationRecord
   belongs_to :user
   belongs_to :shareable, polymorphic: true, counter_cache: :share_count, touch: true
   has_one :location, as: :locateable, dependent: :destroy
-  enum shared_service: [:library, :facebook, :email, :instagram, :imessage, :messenger, :pinterest, :snapchat, :tiktok, :twitter, :whatsapp, :keyboard]
+  enum shared_service: [:library, :facebook, :email, :instagram, :imessage, :messenger, :pinterest, :snapchat, :tiktok, :twitter, :whatsapp, :keyboard, :other]
   after_create do
     shareable.touch
      #invalidate the cache for the shareable assocication
