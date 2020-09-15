9decb3011a922f4c2c7e6d06281f02a43481f3fd
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-09-14 22:44:03 -0400

    Delete the wish.rb and wishlist.rb files

--- app/models/wish.rb
+++
@@ -1,6 +0,0 @@
-class Wish < ApplicationRecord
-  validates :description, length: {minimum:1, maximum:100}
-  belongs_to :user
-  belongs_to :wishlist, counter_cache: :wishes_count
-  has_one_attached :wish_picture #each wish contains a photo showing donators the wish
-end
