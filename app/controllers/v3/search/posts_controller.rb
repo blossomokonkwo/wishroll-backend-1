f366346a3ccd488326aadad354865fcd7999d9b1
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-09-23 13:28:25 -0400

    Change the posts search to be more effiecient by utilizing inner joins down to the active storage blob level.

--- app/controllers/v3/search/posts_controller.rb
+++ app/controllers/v3/search/posts_controller.rb
@@ -3,7 +3,7 @@ class V3::Search::PostsController < ApplicationController
     def index
         limit = 18
         offset = params[:offset]
-        @posts = Post.fetch_multi(Tag.search(params[:q]).offset(offset).limit(limit).pluck(:post_id)).filter {|p| p.media_item.content_type.include?(params["content-type"])}
+        @posts = Post.fetch_multi(Tag.joins(post: {media_item_attachment: :blob}).where("active_storage_blobs.content_type ILIKE ?", "%#{params['content_type']}%").search(params[:q]).limit(limit).offset(offset).pluck(:post_id))
         if @posts.any?
             @current_user = current_user
             render :index, status: :ok
