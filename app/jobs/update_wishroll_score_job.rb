345b1d9e44a0ed5a1b05f86f949a58aa231a614e
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-09-25 12:05:01 -0400

    Use the find method instead of the fetch method to increment the wishroll score

--- app/jobs/update_wishroll_score_job.rb
+++ app/jobs/update_wishroll_score_job.rb
@@ -1,6 +1,6 @@
 class UpdateWishrollScoreJob < ApplicationJob
     def perform(user_id, points)
-        user = User.fetch(user_id)
+        user = User.find(user_id)
         user.wishroll_score += points
         user.save
     end
