5e9a4b1fb7bdd9313fbb1f3c338f6ac598d3cdce
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-09-28 16:45:57 -0400

    Retrived the first element from the document_text array

--- app/controllers/v3/recommendation/posts_controller.rb
+++ app/controllers/v3/recommendation/posts_controller.rb
@@ -3,7 +3,7 @@ class V3::Recommendation::PostsController < ApplicationController
     def index
         limit = 9
         offset = params[:offset]
-        document_text = Post.fetch(params[:post_id]).tags.pluck(:text).flatten
+        document_text = Post.fetch(params[:post_id]).tags.pluck(:text).flatten[0]
         if content_type = params["content-type"] #if the reccommendation is specified by a content-type, search for all similar posts with the specified content type
             @posts = Post.fetch_multi(Tag.joins(post: {media_item_attachment: :blob}).where("active_storage_blobs.content_type ILIKE ?", "%#{content_type}%").recommend(document_text).limit(limit).offset(offset).pluck(:post_id)).uniq {|p| p.id}
         else
