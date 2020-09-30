aed9315be1745e4cd00f5417b1690c047f14e533
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-09-30 14:40:06 -0400

    Remove ts_vector column

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,7 +7,7 @@ class Tag < ApplicationRecord
     validates :text, presence: true
 
     #search API's 
-    pg_search_scope :search, against: :text, using: {tsearch: {dictionary: :english, prefix: true, tsvector_column: :tsv_text}}, order_within_rank: "tags.created_at DESC"
+    pg_search_scope :search, against: :text, using: {tsearch: {dictionary: :english, prefix: true}}, order_within_rank: "tags.created_at DESC"
     pg_search_scope :recommend, against: :text, using: {tsearch: {dictionary: :english, tsvector_column: :tsv_text, any_word: true}}, order_within_rank: "tags.created_at DESC"
 
     after_create do
