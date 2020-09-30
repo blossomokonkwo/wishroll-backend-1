0e73a2bcd1c651de43b724f7eb1237830b1662e9
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-09-30 16:05:40 -0400

    Fix the signup controller

--- app/controllers/v3/signup_controller.rb
+++ app/controllers/v3/signup_controller.rb
@@ -1,14 +1,14 @@
 class V3::SignupController < ApplicationController
     require 'securerandom'
     def new
-        @user = User.new(email: params[:email], password: params[:password], username: "user#{SecureRandom.hex(10)}") #create user w strong params
-        if @user.save #if the user is saved to the DB create tokens and send to client
-            payload = {user_id: @user.id, username: @user.username, is_verified: @user.verified, email: @user.email, full_name: @user.name, profile_picture: @user.avatar_url}
+        @user = User.new(email: params[:email], password: params[:password], username: "user#{SecureRandom.hex(10)}")
+        if @user.save 
+            payload = {id: @user.id, username: @user.username}
             session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
             tokens = session.login
-            render json: {access: tokens[:access], csrf: tokens[:csrf], :access_expires_at => tokens[:access_expires_at]}, status: :created
-        else #if the user isn't saved, return the appropriate error messages to the client
-            render json: @user.errors, status: 400
+            render json: {user: {id: @user.id, username: @user.username, verified: @user.verified, email: @user.email, name: @user.name, avatar: @user.avatar_url, created_at: @user.created_at}, access_token: {access: tokens[:access], csrf: tokens[:csrf], :access_expires_at => tokens[:access_expires_at]}}, status: :created
+        else 
+            render json: @user.errors, status: :bad_request
         end
     end
 end
