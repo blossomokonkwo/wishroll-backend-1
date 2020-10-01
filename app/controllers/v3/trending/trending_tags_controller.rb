188780cec4a9315afb4f6b2c4fa85086a55c1194
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-10-01 04:48:10 -0400

    Set limit for trending tag index to 8

--- app/controllers/v3/trending/trending_tags_controller.rb
+++ app/controllers/v3/trending/trending_tags_controller.rb
@@ -1,7 +1,7 @@
 class V3::Trending::TrendingTagsController < ApplicationController
     before_action :authorize_by_access_header!
     def index
-        @trending_tags = TrendingTag.order(created_at: :desc).offset(params[:offset]).limit(5).to_a
+        @trending_tags = TrendingTag.order(created_at: :desc).offset(params[:offset]).limit(8).to_a
         if @trending_tags.any?
             @current_user = current_user
             render :index, status: :ok  
