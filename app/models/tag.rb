49ced959d5dda8050ffdf6ddfd2092b880dd0025
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-10-02 04:57:32 -0400

    Set anyword to true for new search method

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,7 +7,7 @@ class Tag < ApplicationRecord
 
     #search API's 
     pg_search_scope :search, against: :text, using: {tsearch: {dictionary: :english, normalization: 4, prefix: true, tsvector_column: :tsv_text}}, order_within_rank: "tags.created_at DESC"
-    pg_search_scope :new_search, against: {text: 'A'}, using: {tsearch: {dictionary: :english, tsvector_column: :tsv_text}}, order_within_rank: "tags.created_at DESC"
+    pg_search_scope :new_search, against: {text: 'A'}, using: {tsearch: {dictionary: :english, tsvector_column: :tsv_text, any_word: true}}, order_within_rank: "tags.created_at DESC"
     pg_search_scope :recommend, against: :text, using: {tsearch: {dictionary: :english, tsvector_column: :tsv_text, any_word: true}}, order_within_rank: "tags.created_at DESC"
     pg_search_scope :trending_tag, against: :text, using: {tsearch: {dictionary: :english, tsvector_column: :tsv_text, prefix: true}}, order_within_rank: "tags.created_at ASC"
 
