581986dd25110e99ec73e471d38d2a89875cc7a1
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-10-02 04:12:08 -0400

    Create the search method and add the prefix option

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,8 +6,7 @@ class Tag < ApplicationRecord
     validates :text, presence: true
 
     #search API's 
-    pg_search_scope :search, against: :text, using: {tsearch: {dictionary: :english, normalization: 4, prefix: true, tsvector_column: :tsv_text}}, order_within_rank: "tags.created_at DESC"
-    pg_search_scope :new_search, against: {text: 'A'}, using: {tsearch: {dictionary: :english, tsvector_column: :tsv_text, any_word: true}}, order_within_rank: "tags.created_at DESC"
+    pg_search_scope :search, against: {text: 'A'}, using: {tsearch: {dictionary: :english, tsvector_column: :tsv_text, any_word: true, prefix: true}}, order_within_rank: "tags.created_at DESC"
     pg_search_scope :recommend, against: :text, using: {tsearch: {dictionary: :english, tsvector_column: :tsv_text, any_word: true}}, order_within_rank: "tags.created_at DESC"
     pg_search_scope :trending_tag, against: :text, using: {tsearch: {dictionary: :english, tsvector_column: :tsv_text, prefix: true}}, order_within_rank: "tags.created_at ASC"
 
