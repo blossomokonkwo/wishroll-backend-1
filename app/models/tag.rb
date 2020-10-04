55b39f8f8645fc4fd44c93478cbf5c420de9faad
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-10-04 08:34:58 -0400

    Set normailiztion to 16

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,7 +7,7 @@ class Tag < ApplicationRecord
 
     #search API's 
     pg_search_scope :search, against: :text, using: {tsearch: {dictionary: :english, normalization: 4, prefix: true, tsvector_column: :tsv_text}}, order_within_rank: "tags.created_at DESC"
-    pg_search_scope :new_search, against: :text, using: {tsearch: {tsvector_column: :tsv_text, any_word: true, dictionary: :english}}, order_within_rank: "tags.created_at DESC"
+    pg_search_scope :new_search, against: :text, using: {tsearch: {tsvector_column: :tsv_text, any_word: true, normalization: 16, dictionary: :english}}, order_within_rank: "tags.created_at DESC"
     pg_search_scope :recommend, against: :text, using: {tsearch: {dictionary: :english, tsvector_column: :tsv_text, any_word: true}}, order_within_rank: "tags.created_at DESC"
     pg_search_scope :trending_tag, against: :text, using: {tsearch: {dictionary: :english, tsvector_column: :tsv_text, prefix: true}}, order_within_rank: "tags.created_at ASC"
 
