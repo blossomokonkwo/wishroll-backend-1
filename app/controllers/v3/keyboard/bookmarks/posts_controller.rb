63318f5adf6d80f6a9ce6b21d5bb26350f2819cb
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-10-07 17:43:27 -0400

    Create the keyboard/bookmarks/posts_controller.rb file which allows a specific content type to be specified when requesting the bookmarked posts

---
+++ app/controllers/v3/keyboard/bookmarks/posts_controller.rb
@@ -0,0 +1,14 @@
+class V3::Bookmarks::PostsController < ApplicationController
+    before_action :authorize_by_access_header!
+    def index 
+        offset = params[offset]
+        limit = 15
+        @posts = Post.joins(:bookmarks, media_item_attachment: :blob).where(bookmarks: {user: current_user}).where('active_storage_blobs.content_type ILIKE ?', "%#{params[:content_type]}%").offset(offset).limit(limit).to_a
+        if @posts.any?
+            @current_user = current_user
+            render :index, status: :ok
+        else
+            render json: nil, status: :not_found
+        end
+    end
+end
