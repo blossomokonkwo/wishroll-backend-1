703e3e5c67ffeb30b1b4287dc27bccb9e1a2c2d9
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-10-07 22:14:58 -0400

    Revert last commit

--- app/controllers/v3/keyboard/bookmarks/posts_controller.rb
+++ app/controllers/v3/keyboard/bookmarks/posts_controller.rb
@@ -3,7 +3,7 @@ class V3::Keyboard::Bookmarks::PostsController < ApplicationController
     def index 
         offset = params[offset]
         limit = 15
-        @posts = Post.joins(:bookmarks, media_item_attachment: :blob).where(bookmarks: {user: current_user}).where('active_storage_blobs.content_type ILIKE ?', "%#{params[:content_type]}%").order("bookmarks.created_at DESC").offset(offset).limit(limit).to_a
+        @posts = Post.joins(:bookmarks, media_item_attachment: :blob).where(bookmarks: {user: current_user}).where('active_storage_blobs.content_type ILIKE ?', "%#{params[:content_type]}%").order("bookmarks.created_at").offset(offset).limit(limit).to_a
         if @posts.any?
             @current_user = current_user
             render :index, status: :ok
