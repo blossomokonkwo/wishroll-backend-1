a32f26b4edac5571f552aa1a73567e98513d1e56
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-10-12 01:14:12 -0400

    Change limit to 18

--- app/controllers/v2/trending/posts_controller.rb
+++ app/controllers/v2/trending/posts_controller.rb
@@ -2,7 +2,7 @@ class V2::Trending::PostsController < ApplicationController
     before_action :authorize_by_access_header!
     def trending 
         #the number of posts that are sent to the users feed page. The feed sends the most popular posts
-        limit = 15
+        limit = 18
         offset = params[:offset]
         @posts = Post.order(popularity_rank: :desc).offset(offset).limit(limit).to_a.shuffle!
         if @posts.any?
