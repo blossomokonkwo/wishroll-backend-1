5981dd44c073e05001678d7e9ebb0a9cf5028efb
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-10-21 00:14:01 -0400

    Add User Id attribute

--- app/controllers/v3/discover/rolls_controller.rb
+++ app/controllers/v3/discover/rolls_controller.rb
@@ -4,7 +4,7 @@ class V3::Discover::RollsController < ApplicationController
     def index 
         offset = params[:offset]
         limit = 15
-        @rolls = Roll.select([:caption, :id, :media_url, :thumbnail_url, :popularity_rank, :likes_count, :comments_count, :share_count, :bookmark_count, :view_count, :created_at, :updated_at, :private, :restricted, :uuid])
+        @rolls = Roll.select([:caption, :id, :media_url, :thumbnail_url, :popularity_rank, :likes_count, :comments_count, :share_count, :bookmark_count, :view_count, :created_at, :updated_at, :private, :restricted, :uuid, :user_id])
         .where(private: false, restricted: false).order(popularity_rank: :desc, created_at: :desc)
         .limit(limit).offset(offset)
         if @rolls.any?
