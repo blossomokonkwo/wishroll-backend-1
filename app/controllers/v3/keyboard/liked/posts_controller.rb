43c31e1306be2ca1be553b6355d919ad9465bff1
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-10-27 11:37:27 -0400

    Fix typo in keyboard code

--- app/controllers/v3/keyboard/liked/posts_controller.rb
+++ app/controllers/v3/keyboard/liked/posts_controller.rb
@@ -2,7 +2,7 @@ class V3::Keyboard::Liked::PostsController < ApplicationController
     before_action :authorize_by_access_header!
     
     def index
-        @posts = current_user.liked_posts.(limit: 15, offset: params[:offset]).to_a
+        @posts = current_user.liked_posts(limit: 15, offset: params[:offset]).to_a
         if @posts.any?
             render :index, status: :ok
         else 
