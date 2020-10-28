af37472a68db3d420cfdf0734dd1af20ddcbfda3
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-10-27 20:39:46 -0400

    Finalize Search Keyboard route

--- app/controllers/v3/keyboard/search/posts_controller.rb
+++ app/controllers/v3/keyboard/search/posts_controller.rb
@@ -5,10 +5,10 @@ class V3::Keyboard::Search::PostsController < ApplicationController
         limit = 15
         @posts = Post.fetch_multi(Tag.joins(post: {media_item_attachment: :blob}).where("active_storage_blobs.content_type ILIKE ?", "%#{params['content-type']}%").new_search(params[:q]).limit(limit).offset(offset).pluck(:post_id)).uniq {|p| p.id}
         if @posts.any?
-            SearchActivitiesJob.perform_now(query: params[:q], user_id: current_user.id, content_type: params['content-type'])
+            SearchActivitiesJob.perform_now(query: params[:q], user_id: params[:user_id].to_i, content_type: params['content-type'])
             render :index, status: :ok
         else
-            SearchActivitiesJob.perform_now(query: params[:q], user_id: current_user.id, content_type: nil)
+            SearchActivitiesJob.perform_now(query: params[:q], user_id: params[:user_id].to_i, content_type: nil)
             render json: nil, status: :not_found
         end
     end
