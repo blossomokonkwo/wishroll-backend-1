c44fbf66cffefa3ecd6908c24f243ecfc8d45f97
Great Okonkwo <gboyokonkwo35@gmail.com>
2020-10-31 10:50:59 -0400

    Set the production email configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  #config.action_mailer.default_url_options = {host: '0.0.0.0:3000'}
+  #config.action_mailer.default charset: 'utf-8'
+  #config.action_mailer.delivery_method = 'smtp'
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
