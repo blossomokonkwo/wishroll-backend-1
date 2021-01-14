6c698742273a752081e4890f94cb096ebb37a39f
Great Okonkwo <gboyokonkwo35@gmail.com>
2021-01-14 18:33:13 -0500

    Remove authorization for announcement classes

--- app/controllers/v2/announcements_controller.rb
+++ app/controllers/v2/announcements_controller.rb
@@ -1,6 +1,4 @@
 class V2::AnnouncementsController < ApplicationController
-    before_action :authorize_by_access_header!
-
     def create
         @announcement = Announcement.create
         @announcement.media_item.attach params[:media_item] if params[:media_item]
