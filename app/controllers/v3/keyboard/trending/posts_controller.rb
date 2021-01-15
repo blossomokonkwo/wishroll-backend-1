87e2c962c56f36b5aef96a3b152b2971b625b61e
Great Okonkwo <gboyokonkwo35@gmail.com>
2021-01-14 19:12:49 -0500

    Allow users to access posts via keyboard

--- app/controllers/v3/keyboard/trending/posts_controller.rb
+++ app/controllers/v3/keyboard/trending/posts_controller.rb
@@ -3,7 +3,7 @@ class V3::Keyboard::Trending::PostsController < ApplicationController
         offset = params[:offset]
         limit = 15
         #for now return only WishRoll's posts to the keyboard. Later on when a reccomender system is established, recommended posts will be returned instead
-        @posts = User.fetch(4).posts.order(popularity_rank: :desc).offset(params[:offset]).limit(limit).to_a
+        @posts = Post.order(popularity_rank: :desc).offset(params[:offset]).limit(limit).to_a
         if @posts.any?
             render :index, status: :ok
         else
