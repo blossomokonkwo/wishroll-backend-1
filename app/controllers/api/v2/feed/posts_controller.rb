7520b6ecd4646b547df2db28ca6a25ebc0959b63
Great Okonkwo <gboyokonkwo35@gmail.com>
2021-02-22 00:39:01 -0500

    Fix the post feed algorithm

--- app/controllers/api/v2/feed/posts_controller.rb
+++ app/controllers/api/v2/feed/posts_controller.rb
@@ -4,27 +4,36 @@ class Api::V2::Feed::PostsController < APIController
         begin
             # fetch the access tokens from the request header
             authorize_by_access_header!
+
+            # set the @current_user instance variable
+            @current_user = current_user
+
+            # set the offset and limit
             offset = params[:offset]
-            limit = 18
+            limit = 25
 
             # create an array of the current user's followed users and appen the current user to the array.
-            feed_users = current_user.followed_users.to_a << current_user
+            feed_users = current_user.followed_users.to_a << @current_user
 
             # return all posts created by the users in the feed_users array
-            @posts = Post.includes(:user).where(user: feed_users).order(created_at: :desc).offset(offset).limit(limit)
+            @posts = Post.includes(:user).where(user: feed_users).or(Post.where(restricted: false)).order(created_at: :desc, popularity_rank: :desc).offset(offset).limit(limit)
 
-            # check that posts isn't empty
+            # check that posts array isn't empty
             if @posts.any?
-                @current_user = current_user
                 render :index, status: :ok
             else
-                render json: nil, status: :not_found
+                @posts = Post.where(restricted: false).order(created_at: :desc, popularity_rank: :desc).offset(offset).limit(limit)
+                if @posts.any? 
+                    render :index, status: :ok
+                else
+                    render json: nil, status: :not_found
+                end
             end
+
         rescue => exception
             # handle the case where a user isn't signed in to view their feed maybe recommending posts to the user
-
+            puts exception
             render json: nil, status: :unauthorized
-            
         end
 
     end
