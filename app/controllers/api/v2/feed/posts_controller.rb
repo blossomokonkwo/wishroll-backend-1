c2517d2df419df300b0828f4138000a5eaf832c0
Great Okonkwo <gboyokonkwo35@gmail.com>
2021-04-19 16:13:27 -0400

    Use the offset value provided by the params hash

--- app/controllers/api/v2/feed/posts_controller.rb
+++ app/controllers/api/v2/feed/posts_controller.rb
@@ -16,7 +16,7 @@ class Api::V2::Feed::PostsController < APIController
             feed_users = current_user.followed_users.to_a << @current_user
 
             # return all posts created by the users in the feed_users array
-            @posts = Post.includes(:user).joins(:user).where(user: feed_users).or(Post.where(restricted: false)).where.not(user: @current_user.blocked_users.select(:id)).and(Post.where.not(id: @current_user.reported_posts)).order(created_at: :desc, popularity_rank: :desc).offset(2).limit(25)
+            @posts = Post.includes(:user).joins(:user).where(user: feed_users).or(Post.where(restricted: false)).where.not(user: @current_user.blocked_users.select(:id)).and(Post.where.not(id: @current_user.reported_posts)).order(created_at: :desc, popularity_rank: :desc).offset(offset).limit(25)
 
             # check that posts array isn't empty
             if @posts.any?
