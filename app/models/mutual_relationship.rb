339c2bc70154f583a585b56555b465d74f5390bf
Great Okonkwo <gboyokonkwo35@gmail.com>
2021-08-15 16:07:42 -0400

    Add activity destruction to the after destroy call backs for MutualRelationshipRequest and MutualRelationship objects

--- app/models/mutual_relationship.rb
+++ app/models/mutual_relationship.rb
@@ -4,19 +4,22 @@ class MutualRelationship < ApplicationRecord
   # validations
   validate :mutual_id_and_user_id_must_be_different
 
+  include IdentityCache
+  cache_belongs_to :user
+  cache_belongs_to :mutual
+
   def mutual_id_and_user_id_must_be_different    
     errors.add([:user_id, :mutual_id], "User id and mutual id must be different") if mutual_id == user_id
   end
 
-  after_destroy :invalidate_cache
+  after_destroy :invalidate_cache_and_destroy_activities
 
-  def invalidate_cache
+  private def invalidate_cache_and_destroy_activities
     logger.debug {"[WishRoll Cache] delete succeeded for WishRoll:Cache:MutualRelationship:#{user_id}:Mutual?#{mutual_id}"} if Rails.cache.delete("WishRoll:Cache:MutualRelationship:#{user_id}:Mutual?#{mutual_id}")
+    Activity.where(content_id: id, active_user_id: mutual_id, user_id: user_id, activity_type: self.class.name).first.destroy
   end
   
 
   
-  include IdentityCache
-  cache_belongs_to :user
-  cache_belongs_to :mutual
+
 end
