29fb2585abc1180dd9eae75cfbf65806c2e688b3
Great Okonkwo <gboyokonkwo35@gmail.com>
2021-08-19 20:14:55 -0400

    Use limit variable instead of hard coded value

--- app/controllers/api/v3/trending/posts_controller.rb
+++ app/controllers/api/v3/trending/posts_controller.rb
@@ -12,7 +12,7 @@ class Api::V3::Trending::PostsController < APIController
             offset = params[:offset]
             limit = 15
             trending_users = [5500, 2997, 4493, 123, 392, 4853, 7827, 1798, 5451, 4930, 140, 8667, 749, 1658, 11310, 2856, 1256, 10459, 10224, 362, 2732, 2032, 10023, 3936, 1027, 2931, 5166, 8034, 1608, 5760, 1912, 3044, 2192, 7281, 8715, 7959, 4161, 11924, 2101, 11602, 2976, 9007, 10698, 8569, 4135, 3190, 8093, 80, 1050, 1, 7891, 81, 7403]
-            @posts = Post.joins(:user).where(restricted: false).where(user_id: trending_users).where.not(user: @current_user.blocked_users.select(:id)).and(Post.where.not(id: @current_user.reported_posts)).order(created_at: :desc, popularity_rank: :desc).offset(offset).limit(15) 
+            @posts = Post.joins(:user).where(restricted: false).where(user_id: trending_users).where.not(user: @current_user.blocked_users.select(:id)).and(Post.where.not(id: @current_user.reported_posts)).order(created_at: :desc, popularity_rank: :desc).offset(offset).limit(limit) 
 
             # check that posts array isn't empty
             if @posts.any?
