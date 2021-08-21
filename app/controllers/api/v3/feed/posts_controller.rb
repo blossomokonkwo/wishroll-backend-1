4dc501889b7a386693a53e24d6cef2b15a0bbff0
Great Okonkwo <gboyokonkwo35@gmail.com>
2021-08-21 01:14:18 -0400

    Return a users mutuals post on the feed page

--- app/controllers/api/v3/feed/posts_controller.rb
+++ app/controllers/api/v3/feed/posts_controller.rb
@@ -13,7 +13,7 @@ class Api::V3::Feed::PostsController < APIController
             limit = params[:limit] || 12
 
             # create an array of the current user's followed users and appen the current user to the array.
-            feed_users = current_user.followed_users.to_a << @current_user
+            feed_users = current_user.mutuals.to_a << @current_user
 
             # return all posts created by the users in the feed_users array
             @posts = Post.joins(:user).where(user: feed_users).where.not(user: @current_user.blocked_users.select(:id)).and(Post.where.not(id: @current_user.reported_posts)).order(created_at: :desc).offset(offset).limit(limit)
