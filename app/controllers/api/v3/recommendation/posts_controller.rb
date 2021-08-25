d91b1e26adc8e19cd3b2608a5793d59738eb96d5
Great Okonkwo <gboyokonkwo35@gmail.com>
2021-08-24 23:44:28 -0400

    Change recommendation controller

--- app/controllers/api/v3/recommendation/posts_controller.rb
+++ app/controllers/api/v3/recommendation/posts_controller.rb
@@ -8,7 +8,7 @@ class Api::V3::Recommendation::PostsController < ApplicationController
         if content_type = params["content-type"] #if the reccommendation is specified by a content-type, search for all similar posts with the specified content type
             @posts = Post.where(id: Tag.joins(post: {media_item_attachment: :blob}).where("active_storage_blobs.content_type ILIKE ? AND posts.id != ?", "%#{content_type}%", post_id).recommend(document_text).limit(limit).offset(offset).pluck(:post_id)).and(Post.where.not(id: current_user.reported_posts)).and(Post.where.not(user: current_user.blocked_users)).and(Post.where.not(user: current_user.blocker_users)).distinct
         else
-            @posts = Post.includes([:board, :user]).where(id: Tag.joins(:post).where("posts.id != ?", post_id).recommend(document_text).limit(limit).offset(offset).pluck(:post_id))
+            @posts = Post.includes([:user]).where(id: Tag.joins(:post).where("posts.id != ?", post_id).recommend(document_text).pluck(:post_id))
             .and(Post.where.not(id: current_user.reported_posts))
             .and(Post.where.not(user: current_user.blocked_users))
             .and(Post.where.not(user: current_user.blocker_users))
